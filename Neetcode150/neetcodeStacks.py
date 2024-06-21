eeb09e839a98bf2a72e944793fb7472e402565f2
Aneek Roy <aneek.roy@xerionbattery.com>
2024-06-21 09:45:41 -0400

    AR

--- Neetcode150/neetcodeStacks.py
+++ Neetcode150/neetcodeStacks.py
@@ -1,15 +1,25 @@
 
+# Validate Parentheses
 
 def isValid(s):
 
+    stack = []
+
     checker = {
-            '}' : '{',
-            ')' : '(',
-            ']' : '['
-        }
+        '}' : '{',
+        ')' : '(',
+        ']' : '['
+    }
 
-        stack = []
+    # Check 1: If the length of the string is odd, then it is automatically invalid
+    if len(s) % 2 != 0: 
+        return False
 
-        for j in s:
-            if j not in stack:
-                stack.append(j)
+    # Logic, first half goes in the stack, then the second half should check the dictionary for 
+    for j in s:
+        if j in checker:
+            print("inside")
+            
+        
+    
+is
