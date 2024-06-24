4d0b33e8b4bb90a24d0ddb96ec34280f81787e07
Aneek Roy <aneek.roy@xerionbattery.com>
2024-06-24 16:19:32 -0400

    AR

--- Neetcode150/neetcodeStacks.py
+++ Neetcode150/neetcodeStacks.py
@@ -54,7 +54,15 @@ class MinStack:
 
 # Evaluate Reverse Polish Notation
 
+
+
 def evalRPN(tokens):
+    """"You are given an array of strings tokens that represents a valid arithmetic expression in Reverse Polish Notation.
+        Return the integer that represents the evaluation of the expression.
+
+        The operands may be integers or the results of other operations.
+        The operators include '+', '-', '*', and '/'.
+        Assume that division between integers always truncates toward zero."""
     fin = 0 
     stack = []
     operators = ['+', '-', '*', '/']
