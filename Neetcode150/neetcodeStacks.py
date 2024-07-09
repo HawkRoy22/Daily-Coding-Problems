c3dd74b89033067c2b0f9ce2d191608f4e2ec63a
Aneek Roy <aneek.roy@xerionbattery.com>
2024-07-09 09:12:51 -0400

    AR

--- Neetcode150/neetcodeStacks.py
+++ Neetcode150/neetcodeStacks.py
@@ -1,6 +1,9 @@
 
 # Validate Parentheses
 
+import math
+
+
 def isValid(s):
 
     stack = []
@@ -63,25 +66,68 @@ def evalRPN(tokens):
         The operands may be integers or the results of other operations.
         The operators include '+', '-', '*', and '/'.
         Assume that division between integers always truncates toward zero."""
-    fin = 0 
-    stack = []
-    operators = ['+', '-', '*', '/']
+    sumOf = 0
 
+    stack = []
+    
     for j in tokens:
-        if j not in operators:
-            stack.append(j)
-            #stack.insert(0, j)
+        if j.lstrip('-').isdigit():
+            stack.append(int(j))
         else:
-            match j:
-                case '+':
-                    fin += stack[0] + stack[1]
-                    stack.clear()
-                case '-':
-                    fin += stack[0] - stack[1]
-                    stack.clear()
-                case '*':
-                    fin += stack[0] * stack[1]
-                    stack.clear()
-                case '/':
-                    fin += stack[0] / stack[1]
-                    stack.clear()
+            if j == '+':
+                sumOf = stack[-1] + stack[-2]
+            elif j == '-':
+                sumOf =stack[-1]- stack[-2]
+            elif j == '*':
+                sumOf = stack[-2] * stack[-1]
+            elif j == '/':
+                sumOf = int((stack[-2] / stack[-1]))
+
+            stack.pop()
+            stack.pop()
+            stack.append(sumOf)
+            print("Stack : ", stack)
+
+
+    print(stack[0])
+
+    return stack[0]
+
+#evalRPN(["2","1","+","3","*"])
+#evalRPN(["4","13","5","/","+"])
+
+#evalRPN(["10","6","9","3","+","-11","*","/","*","17","+","5","+"])
+
+
+
+def dailyTemperatures(temperatures):
+    result = []
+    l = 0 
+    r = 1
+    counter = 0
+
+    while l < len(temperatures):
+
+        if r >= len(temperatures):
+            l += 1
+            r = l + 1
+            counter = 0
+            result.append(counter)
+
+        elif temperatures[r] > temperatures[l]:
+            counter += 1
+            result.append(counter)
+            counter = 0 
+            l += 1
+            r = l + 1
+        else:
+            counter += 1
+            r += 1
+    
+    print(result)
+
+#dailyTemperatures([30,38,30,36,35,40,2])
+#dailyTemperatures([22,21,20])
+dailyTemperatures([55,38,53,81,61,93,97,32,43,78])
+
+
